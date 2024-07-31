7943ac3fabba520c337f81b1728a72c41b07873c
Nitin Kumar <2250606@tcs.com>
2024-07-31 15:47:18 +0530

    Commiting configuration server

---
+++ ConfigServer/src/test/java/com/micro/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
